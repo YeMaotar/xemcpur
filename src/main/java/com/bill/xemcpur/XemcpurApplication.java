54a92f07214c24a4428d1294efdec1b41f1e8823
ZhangSan <31501054+YeMaotar@users.noreply.github.com>
2022-08-22 11:27:09 +0800

    初始化

---
+++ src/main/java/com/bill/xemcpur/XemcpurApplication.java
@@ -0,0 +1,13 @@
+package com.bill.xemcpur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XemcpurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XemcpurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bill/xemcpur/XemcpurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.xemcpur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XemcpurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
