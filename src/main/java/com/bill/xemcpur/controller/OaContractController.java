9c24ad4bb028c29f27df308eafe58e0ae94dae2e
xuchao <email>
2022-08-31 15:58:24 +0800

    代码提交
    Signed-off-by: xuchao <email>

--- src/main/java/com/bill/xemcpur/controller/OaContractController.java
+++ src/main/java/com/bill/xemcpur/controller/OaContractController.java
@@ -1,7 +1,8 @@
 package com.bill.xemcpur.controller;
-import java.io.*;
-import java.nio.charset.Charset;
-import com.alibaba.fastjson.JSONArray;
+
+import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSON;
+import com.bill.xemcpur.utils.RestClientUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -15,33 +16,37 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import com.alibaba.druid.support.json.JSONUtils;
-import com.alibaba.fastjson.JSON;
-import com.bill.xemcpur.utils.RestClientUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 /**
- * 业务单据接口入口(请购单，NC采购合同，年度计划，采购平台采购合同)
- * @author zhangzhw
- * @date 2022-06-21
+ * @title: FileController
+ * @Author ZhangZw
+ * @Date: 2022/4/25 15:56
+ * @Version 1.0
+ * 文件上传接口
  */
 @RestController
-@RequestMapping("transit")
-public class OaTransitController {
-    String url="http://192.180.4.14/";
-    String tabName="formmain_1582";
+@RequestMapping("/OaContract")
+public class OaContractController {
+    //String url="http://192.180.4.14/";  //OA地址
+    String tabName="formmain_1582";    //OA流程表单名称
+    String url="http://127.0.0.1:8097/";   //OA地址
+    //String tabName="formmain_0116"; 本机
+    String fileSite="C:\\Users\\Timber\\Desktop\\OEFile\\";  //文件保存地址
 
-    //http://127.0.0.1:8097/
-    //formmain_0116  本机
 
     /**
      * 上传文件数据到OA
@@ -49,11 +54,11 @@ public class OaTransitController {
      * @return
      */
     @PostMapping(value = "/upload")
-    public String upLoadFiles(@RequestBody MultipartFile multipartFile) throws Exception{
+    public String upLoadFiles(@RequestParam("multipartFile") MultipartFile multipartFile) throws Exception{
         File file=multipartFileToFile(multipartFile);
         //删除缓存信息
         try {
-            String str= fileDataRequest(url+"seeyon/rest/attachment?token="+getToken(), "{\"1\":1}",file);
+            String str= RestClientUtils.fileDataRequest(url+"seeyon/rest/attachment?token="+getToken(), "{\"1\":1}",file);
             Map<String, Object> returnData = JSON.parseObject(str, HashMap.class);
             Map<String,Object> data0=JSON.parseObject(((List<Object>)returnData.get("atts")).get(0).toString());
             System.out.println(data0.get("fileUrl"));
@@ -72,46 +77,31 @@ public class OaTransitController {
         }
         return null;
     }
-    /**
-     *
-     * 调用带文件的数据接口请求
-     * @param url
-     * @param data
-     * @return
-     *
-     */
 
-    public String fileDataRequest(String url, String data, File pdfFile)
-            throws ClientProtocolException, ParseException, IOException {
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        try {
-            httpClient = HttpClients.createDefault();
-            StringBody requestData = new StringBody(data, ContentType.create("text/plain", Consts.UTF_8));
-            FileBody bin = new FileBody(pdfFile);
-            HttpEntity entity = MultipartEntityBuilder.create()
-                    .addPart("file", bin)
-                    .addPart("requestData", requestData)
-                    .build();
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.setEntity(entity);
-            // 发起请求 并返回请求的响应
-            response = httpClient.execute(httpPost);
-            // 获取响应对象
-            HttpEntity resEntity = response.getEntity();
-            String responseStr = EntityUtils.toString(response.getEntity(), Charset.forName("UTF-8"));
-            // 销毁
-            EntityUtils.consume(resEntity);
-            return responseStr;
-        } finally {
-            if (response != null) {
-                response.close();
-            }
-            if (httpClient != null) {
-                httpClient.close();
-            }
-        }
+
+    /**
+     * 提供id下载文件
+     * @return
+     */
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam(value="id") String id)
+            throws IOException {
+        String filePath = fileSite+getCurrTime()+id;
+        FileSystemResource file = new FileSystemResource(filePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", new String(id.getBytes("UTF-8"),"ISO-8859-1")));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
     }
+
 
 
     /**
@@ -122,14 +112,14 @@ public class OaTransitController {
     @PostMapping(value = "/getToken")
     public String getToken() throws Exception{
         Map<String, String> myMap = new HashMap<String, String>();
-        myMap.put("userName", "xd-admin");
-        myMap.put("password", "ae578eee-acff-4b75-9aea-169155531c0f");
-        myMap.put("loginName", "liuxuan");
-        //myMap.put("userName", "rest");
-        //myMap.put("password", "01e2acf7-1ae5-45a7-864c-3958d15b5063");
-        //myMap.put("loginName", "timber");
+        //myMap.put("userName", "xd-admin");
+        //myMap.put("password", "ae578eee-acff-4b75-9aea-169155531c0f");
+        //myMap.put("loginName", "liuxuan");
+        myMap.put("userName", "rest");
+        myMap.put("password", "01e2acf7-1ae5-45a7-864c-3958d15b5063");
+        myMap.put("loginName", "timber");
         String json= JSONUtils.toJSONString(myMap);
-        String  retdata =RestClientUtils.doPostJson(url+"seeyon/rest/token", json);
+        String  retdata =RestClientUtils.doPostJsonToOa(url+"seeyon/rest/token", json);
         Map<String, Object> returnData = JSON.parseObject(retdata, HashMap.class);
         return returnData.get("id").toString();
     }
@@ -164,7 +154,7 @@ public class OaTransitController {
         mainData.put("合同来源", data.get("field0011"));
         mainData.put("备注", data.get("field0012"));
         mainData.put("附件ID", data.get("field0013"));
-        mainData.put("合同附件", "8694702911343250661");
+        //mainData.put("合同附件", "8694702911343250661");
         HashMap<String, Object> thirdAttachment =new HashMap<String, Object>();
         thirdAttachment.put("subReference", 8694702911343250661L);
         thirdAttachment.put("fileUrl", -700452078380429737L);
@@ -172,11 +162,11 @@ public class OaTransitController {
         List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
         list.add(thirdAttachment);
         dataTwo.put(tabName, mainData);
-        dataTwo.put("thirdAttachments", list);
+        //dataTwo.put("thirdAttachments", list);
         dataOne.put("data", dataTwo);
         map.put("data", dataOne);
         String json= JSONUtils.toJSONString(map);
-        String  retdata =RestClientUtils.doPostJson(url+"seeyon/rest/bpm/process/start?token="+getToken(), json);
+        String  retdata =RestClientUtils.doPostJsonToOa(url+"seeyon/rest/bpm/process/start?token="+getToken(), json);
         return  retdata.toString();
     }
 
@@ -188,23 +178,26 @@ public class OaTransitController {
      */
     @PostMapping(value = "/writeBackToEproc")
     public Map<String,Object> writeBackToEproc(@RequestBody Map<String, Object> data) throws Exception{
+        String filepath = fileSite+getCurrTime();
         Long id=(Long) data.get("id");
         String filename=(String) data.get("filename");
         // 创建 RestTemplate
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         //get请求参数
-        headers.add("token","a01fce98-9d3d-453d-8b8d-d938ef4e82b7");
+        headers.add("token",getToken());
         headers.add("fileName",filename);
         // 调用下载接口进行下载
         // id 为String类型，为被调用接口参数
-        ResponseEntity<byte[]> entity = restTemplate.exchange("http://127.0.0.1:8097//seeyon/rest/attachment/file/"+id, HttpMethod.GET,new org.springframework.http.HttpEntity<>(headers), byte[].class);
+        ResponseEntity<byte[]> entity = restTemplate.exchange(url+"seeyon/rest/attachment/file/"+id, HttpMethod.GET,new org.springframework.http.HttpEntity<>(headers), byte[].class);
         // 返回数据，在下面写到输出流里面（由于我是需要把字节数组传递给另外一个系统，所以我注释了下面的代码，我不需要存本地）
         byte[] body = entity.getBody();
 
         if(body != null) {
+            //判断文件夹是否存在，不存在则创建一个 一个月一个文件夹
+            judgeSite(filepath);
             // 存到本地
-            String filepath = filename;
+            filepath= filepath + id+"." +getFileSffix(filename);
             File file = new File(filepath);
             if (file.exists()) {
                 file.delete();
@@ -213,32 +206,7 @@ public class OaTransitController {
             fos.write(body, 0, body.length);
             fos.flush();
             fos.close();
-
-
-            try {
-                //调用E采购回写接口
-                String str= fileDataRequest(url+"seeyon/rest/attachment?token="+getToken(), "{\"1\":1}",file);
-                Map<String, Object> returnData = JSON.parseObject(str, HashMap.class);
-                Map<String,Object> data0=JSON.parseObject(((List<Object>)returnData.get("atts")).get(0).toString());
-                System.out.println(data0.get("fileUrl"));
-                //删除本地文件
-                file.delete();
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (ParseException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-
         }
-
-
-
         Map<String,Object> rdata=new HashMap<String, Object>();
         rdata.put("resCode","0");
         rdata.put("resMsg","回写成功");
@@ -255,10 +223,9 @@ public class OaTransitController {
      * @throws Exception
      */
     @PostMapping(value = "/queryPersonnel")
-    public List<Map> queryPersonnel() throws Exception{
-        String  retdata =RestClientUtils.doPostJson(url+"seeyon/rest/personnel/query?token="+getToken(), "");
-        List<Map> list= JSONArray.parseArray(retdata,Map.class);
-        return list;
+    public String queryPersonnel(@RequestBody Map<String, Object> data) throws Exception{
+        String  retdata =RestClientUtils.doPostJsonToOa(url+"seeyon/rest/personnel/query?token="+getToken(), JSONUtils.toJSONString(data));
+        return retdata;
     }
 
 
@@ -319,6 +286,40 @@ public class OaTransitController {
         return split;
     }
 
+    public static void main(String[] args) {
+        try {
+            Map<String, Object> tabMap = new HashMap<String, Object>();
+            tabMap.put("id",  -7837679920534370626L);
+            tabMap.put("filename",  "2016制造费用.xls");
+            new OaContractController().writeBackToEproc(tabMap);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * 判断指定文件夹是否存在
+     */
+    public void judgeSite(String site){
+        File file = new File(site);
+        if(!file.exists()&& !file.isDirectory()){
+            file.mkdirs();                        //把a.sql也当做文件夹名来创建
+            file.getParentFile().mkdirs();       //获取路径的根路径领创建文件，到最后最后一级路径前E:\新建文件夹\20211110\
+        }
+    }
+
+
+    public  String getCurrTime(){
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMM");
+        return simpleDateFormat.format(new Date())+"\\";
+    }
+
+
+    public String getFileSffix(String filename){
+        String[] strs=filename.split("\\.");
+        return strs[strs.length-1];
+    }
 
 }
-
--- src/main/java/com/bill/xemcpur/utils/RestClientUtils.java
+++ src/main/java/com/bill/xemcpur/utils/RestClientUtils.java
@@ -1,14 +1,23 @@
 package com.bill.xemcpur.utils;
 
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  *
@@ -46,5 +55,80 @@ public class RestClientUtils {
             }
         }
         return resultString;
+    }
+
+
+    /**
+     * 传送json类型的post请求
+     * @param url
+     * @param json
+     * @return String
+     */
+    public static String doPostJsonToOa(String url, String json) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            // 创建请求内容
+            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultString;
+    }
+
+
+    /**
+     *
+     * 调用带文件的数据接口请求
+     * @param url
+     * @param data
+     * @return
+     *
+     */
+
+    public static String fileDataRequest(String url, String data, File pdfFile)
+            throws ClientProtocolException, ParseException, IOException {
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        try {
+            httpClient = HttpClients.createDefault();
+            StringBody requestData = new StringBody(data, ContentType.create("text/plain", Consts.UTF_8));
+            FileBody bin = new FileBody(pdfFile);
+            HttpEntity entity = MultipartEntityBuilder.create()
+                    .addPart("file", bin)
+                    .addPart("requestData", requestData)
+                    .build();
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setEntity(entity);
+            // 发起请求 并返回请求的响应
+            response = httpClient.execute(httpPost);
+            // 获取响应对象
+            HttpEntity resEntity = response.getEntity();
+            String responseStr = EntityUtils.toString(response.getEntity(), Charset.forName("UTF-8"));
+            // 销毁
+            EntityUtils.consume(resEntity);
+            return responseStr;
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+            if (httpClient != null) {
+                httpClient.close();
+            }
+        }
     }
 }
